b88079ef165fe97027764c5b3d3f7dff3d965205
BenedettoUni <benedetto.tessarini@studenti.univr.it>
2025-09-02 15:30:03 +0200

    fatta parte del training, modificato gitignore

---
+++ src/cifar_train.py
@@ -0,0 +1,68 @@
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import CIFAR10
+import lightning as L
+
+# Encoder e Decoder semplici
+class Encoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(3*32*32, 128)
+        self.fc2 = nn.Linear(128, 3)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(3, 128)
+        self.fc2 = nn.Linear(128, 3*32*32)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+#Lightning
+class LitAutoEncoder(L.LightningModule):
+    def __init__(self, encoder, decoder):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def training_step(self, batch, batch_idx):
+        x, _ = batch
+        x = x.view(x.size(0), -1)
+        z = self.encoder(x)        
+        x_hat = self.decoder(z)    
+        loss = F.mse_loss(x_hat, x)
+        self.log("train_loss", loss) 
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=1e-3)
+
+
+# Dataset e DataLoader
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))
+])
+
+dataset = CIFAR10(root=os.getcwd(), train=True, download=True, transform=transform)
+train_loader = DataLoader(dataset, batch_size=64, shuffle=True)
+
+# Creazione modello e training
+encoder = Encoder()
+decoder = Decoder()
+autoencoder = LitAutoEncoder(encoder, decoder)
+
+trainer = L.Trainer(max_epochs=5) 
+trainer.fit(autoencoder, train_dataloaders=train_loader)
